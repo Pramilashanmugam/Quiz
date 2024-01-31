cff84ceb61cbdb4d05c0434bf9b91d85966072ab
Pramila shanmugam <pramila.sl@gmail.com>
2024-01-31 15:10:24 +0000

    quiz div styled

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,31 @@
 function goToInstructions() {
 
-    let swap = document.getElementById('container').style.display !== 'none';   
-    if(swap == true){
-        document.getElementById('container').style.display= 'none';
+    let swap = document.getElementById('container').style.display !== 'none';
+    if (swap == true) {
+        document.getElementById('container').style.display = 'none';
         document.getElementById('instructions').style.display = 'block';
-    }else{
-        document.getElementById('container').style.display= 'block';
+    } else {
+        document.getElementById('container').style.display = 'block';
         document.getElementById('instructions').style.display = 'none';
     }
-   
-        
+
+
 }
 
 function jumpToOptions() {
-    let swap = document.getElementById('instructions').style.display !== 'none'; 
-    alert(swap);
-    if(swap == true){
-        document.getElementById('instructions').style.display= 'none';
+    let swap = document.getElementById('instructions').style.display !== 'none';
+    if (swap == true) {
+        document.getElementById('instructions').style.display = 'none';
         document.getElementById('options').style.display = 'block';
     }
 }
 
 function htmlQuizQuestions() {
     alert('quiz started');
+    let swap = document.getElementById('options').style.display !== 'none';
+    if (swap == true) {
+        document.getElementById('options').style.display = 'none';
+        document.getElementById('quiz').style.display = 'block';
+    }
+
 }
