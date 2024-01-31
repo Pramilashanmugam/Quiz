6ce3aab750817247efa4966d30c7daafc1a41b8b
Pramila shanmugam <pramila.sl@gmail.com>
2024-01-31 15:34:31 +0000

    Html questions & answer array created

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,73 @@ function htmlQuizQuestions() {
     }
 
 }
+let htmlQuestion = document.getElementById('question');
+let htmlAnswer = document.getElementById('answers');
+let nextButton = document.getElementById('nxt-button');
+
+var questions = [
+    {
+        question:"What does HTML stand for?",
+        answer: [
+            {text:'Hyper Text Markup Language', correct: true},
+            {text:'Home Tool Markup Language', correct: false},
+            {text:'Hyperlinks and Text Markup Language', correct: false},
+            {text:'Hyper Type Markup Language', correct: false},
+        ]
+    },
+    {
+        question:"Which character is used to indicate an end tag?",
+        answer: [
+            {text:'<', correct: true},
+            {text:'*', correct: false},
+            {text:'^', correct: false},
+            {text:'/', correct: true},
+        ]
+    },
+    {
+        question:"Who is making the Web standards?",
+        answer: [
+            {text:'The World Wide Web Consortium', correct: true},
+            {text:'Mozilla', correct: false},
+            {text:'Google', correct: false},
+            {text:'Microsoft', correct: false},
+        ]
+    },
+    {
+        question:"Choose the correct HTML element for the largest heading:",
+        answer: [
+            {text:'<head>', correct: false},
+            {text:'<heading>', correct: false},
+            {text:'<h1>', correct: true},
+            {text:'<h6>', correct: false},
+        ]
+    },
+    {
+        question:"Choose the correct HTML element to define important text",
+        answer: [
+            {text:'<important>', correct: false},
+            {text:'<strong>', correct: true},
+            {text:'<i>', correct: false},
+            {text:'<b>', correct: false},
+        ]
+    }
+];
+
+function cssQuizQuestions() {
+    alert('quiz started');
+    let swap = document.getElementById('options').style.display !== 'none';
+    if (swap == true) {
+        document.getElementById('options').style.display = 'none';
+        document.getElementById('quiz').style.display = 'block';
+    }
+
+}
+function javascriptQuizQuestions() {
+    alert('quiz started');
+    let swap = document.getElementById('options').style.display !== 'none';
+    if (swap == true) {
+        document.getElementById('options').style.display = 'none';
+        document.getElementById('quiz').style.display = 'block';
+    }
+
+}
