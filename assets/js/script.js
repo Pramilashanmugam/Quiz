2a3a95cb0bdfee62606c78daabeaf8fb1c7d707e
Pramila shanmugam <pramila.sl@gmail.com>
2024-02-03 15:37:31 +0000

    func for start and display quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,60 +26,85 @@ function htmlQuizQuestions() {
     if (swap == true) {
         document.getElementById('options').style.display = 'none';
         document.getElementById('quiz').style.display = 'block';
+        startQuiz();
     }
-
 }
-let htmlQuestion = document.getElementById('question');
-let htmlAnswer = document.getElementById('answers');
-let nextButton = document.getElementById('nxt-button');
 
 var questions = [
     {
-        question:"What does HTML stand for?",
+        question: "What does HTML stand for?",
         answer: [
-            {text:'Hyper Text Markup Language', correct: true},
-            {text:'Home Tool Markup Language', correct: false},
-            {text:'Hyperlinks and Text Markup Language', correct: false},
-            {text:'Hyper Type Markup Language', correct: false},
+            { text: 'Hyper Text Markup Language', correct: true },
+            { text: 'Home Tool Markup Language', correct: false },
+            { text: 'Hyperlinks and Text Markup Language', correct: false },
+            { text: 'Hyper Type Markup Language', correct: false },
         ]
     },
     {
-        question:"Which character is used to indicate an end tag?",
+        question: "Which character is used to indicate an end tag?",
         answer: [
-            {text:'<', correct: true},
-            {text:'*', correct: false},
-            {text:'^', correct: false},
-            {text:'/', correct: true},
+            { text: '<', correct: true },
+            { text: '*', correct: false },
+            { text: '^', correct: false },
+            { text: '/', correct: true },
         ]
     },
     {
-        question:"Who is making the Web standards?",
+        question: "Who is making the Web standards?",
         answer: [
-            {text:'The World Wide Web Consortium', correct: true},
-            {text:'Mozilla', correct: false},
-            {text:'Google', correct: false},
-            {text:'Microsoft', correct: false},
+            { text: 'The World Wide Web Consortium', correct: true },
+            { text: 'Mozilla', correct: false },
+            { text: 'Google', correct: false },
+            { text: 'Microsoft', correct: false },
         ]
     },
     {
-        question:"Choose the correct HTML element for the largest heading:",
+        question: "Choose the correct HTML element for the largest heading:",
         answer: [
-            {text:'<head>', correct: false},
-            {text:'<heading>', correct: false},
-            {text:'<h1>', correct: true},
-            {text:'<h6>', correct: false},
+            { text: '<head>', correct: false },
+            { text: '<heading>', correct: false },
+            { text: '<h1>', correct: true },
+            { text: '<h6>', correct: false },
         ]
     },
     {
-        question:"Choose the correct HTML element to define important text",
+        question: "Choose the correct HTML element to define important text",
         answer: [
-            {text:'<important>', correct: false},
-            {text:'<strong>', correct: true},
-            {text:'<i>', correct: false},
-            {text:'<b>', correct: false},
+            { text: '<important>', correct: false },
+            { text: '<strong>', correct: true },
+            { text: '<i>', correct: false },
+            { text: '<b>', correct: false },
         ]
     }
 ];
+
+let htmlQuestion = document.getElementById('question');
+let htmlAnswer = document.getElementById('answers');
+let nextButton = document.getElementById('nxt-button');
+var currentQuestionIndex;
+var score;
+
+function startQuiz() {
+    alert('check');
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML="Next";
+    displayQuiz()
+}
+
+function displayQuiz() {
+    
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNum = currentQuestionIndex + 1;
+    htmlQuestion.innerHTML= questionNum + '. ' + currentQuestion.question;
+
+    currentQuestion.answer.forEach(answer => {
+        let button = document.createElement('button');
+        button.innerHTML = answer.text;
+        button.classList.add('ans-button');
+        htmlAnswer.appendChild(button);
+    });
+}
 
 function cssQuizQuestions() {
     alert('quiz started');
@@ -87,6 +112,7 @@ function cssQuizQuestions() {
     if (swap == true) {
         document.getElementById('options').style.display = 'none';
         document.getElementById('quiz').style.display = 'block';
+       
     }
 
 }
@@ -96,6 +122,7 @@ function javascriptQuizQuestions() {
     if (swap == true) {
         document.getElementById('options').style.display = 'none';
         document.getElementById('quiz').style.display = 'block';
+       
     }
 
 }
