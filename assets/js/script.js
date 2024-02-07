3d5c9acf20e2663f99fd03638ab76a68385943fe
Pramila shanmugam <pramila.sl@gmail.com>
2024-02-07 14:55:10 +0000

    question and answer set for complete quiz updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,6 +65,51 @@ let htmlQuestions = [
             { text: '&lt;i&gt;', correct: false },
             { text: '&lt;b&gt;', correct: false },
         ]
+    },
+    {
+        question: "Which of these elements are all &lt;table&gt; elements?",
+        answers: [
+            { text: '&lt;thead&gt;&lt;body&gt;&lt;tr&gt;', correct: false },
+            { text: '&lt;table&gt;&lt;tr&gt;&lt;td&gt;', correct: true },
+            { text: '&lt;table&gt;&lt;head&gt;&lt;tfoot&gt;', correct: false },
+            { text: '&lt;table&gt;&lt;tr&gt;&lt;tt&gt;', correct: false },
+        ]
+    },
+    {
+        question: "How can you make a numbered list?",
+        answers: [
+            { text: '&lt;list&gt;', correct: false },
+            { text: '&lt;dl&gt;', correct: false },
+            { text: '&lt;ol&gt;', correct: true },
+            { text: '&lt;ul&gt;', correct: false },
+        ]
+    },
+    {
+        question: "What is the correct HTML for making a checkbox?",
+        answers: [
+            { text: '<checkbox>', correct: false },
+            { text: '<input type="checkbox">', correct: true },
+            { text: '<input type="check">', correct: false },
+            { text: '<check>', correct: false },
+        ]
+    },
+    {
+        question: "What is the correct HTML for inserting an image?",
+        answers: [
+            { text: '&lt;img src="image.gif" alt="MyImage"&gt;', correct: true },
+            { text: '&lt;image src="image.gif" alt="MyImage"&gt;', correct: false },
+            { text: '&lt;img href="image.gif" alt="MyImage"&gt;', correct: false },
+            { text: '&lt;img alt="MyImage"&gt;image.gif&lt;/img&gt;', correct: false },
+        ]
+    },
+    {
+        question: "In HTML, which attribute is used to specify that an input field must be filled out?",
+        answers: [
+            { text: 'required', correct: true },
+            { text: 'placeholder', correct: false },
+            { text: 'validate', correct: false },
+            { text: 'formvalidate', correct: false },
+        ]
     }
 ];
 // html quiz array ends here
@@ -72,48 +117,93 @@ let htmlQuestions = [
 // question and answer array for CSS quiz 
 let cssQuestions = [
     {
-        question: "CCS q1?",
+        question: "What does CSS stand for?",
         answers: [
-            { text: 'CCS q1', correct: true },
-            { text: 'Home Tool Markup Language', correct: false },
-            { text: 'Hyperlinks and Text Markup Language', correct: false },
-            { text: 'Hyper Type Markup Language', correct: false },
+            { text: 'Creative Style Sheets', correct: false },
+            { text: 'Computer Style Sheets', correct: false },
+            { text: 'Colorful Style Sheets', correct: false },
+            { text: 'Cascading Style Sheets', correct: true },
         ]
     },
     {
-        question: "CCS q2?",
+        question: "What is the correct HTML for referring to an external style sheet?",
         answers: [
-            { text: '<', correct: false },
-            { text: '*', correct: false },
-            { text: '^', correct: false },
-            { text: 'CCS q2', correct: true },
+            { text: '&lt;link rel="stylesheet" type="text/css" href="mystyle.css"&gt;', correct: true },
+            { text: '&lt;stylesheet&gt;mystyle.css&lt;/stylesheet&gt;', correct: false },
+            { text: '&lt;style src="mystyle.css"&gt;', correct: false },
+            { text: '&lt;stylesheet src="mystyle.css"&gt;', correct: false },
         ]
     },
     {
-        question: "CCS q3?",
+        question: "Which HTML attribute is used to define inline styles?",
         answers: [
-            { text: 'CCS q3', correct: true },
-            { text: 'Mozilla', correct: false },
-            { text: 'Google', correct: false },
-            { text: 'Microsoft', correct: false },
+            { text: 'font', correct: false },
+            { text: 'style', correct: true },
+            { text: 'styles', correct: false },
+            { text: 'Class', correct: false },
         ]
     },
     {
-        question: "CCS q4",
+        question: "What is the default value of the position property?",
         answers: [
-            { text: '&lt;head&gt;', correct: false },
-            { text: "&lt;heading&gt;", correct: false },
-            { text: 'CCS q4', correct: true },
-            { text: '&lt;h6&gt;', correct: false },
+            { text: 'static', correct: true },
+            { text: "fixed", correct: false },
+            { text: 'relative', correct: false },
+            { text: 'Absolute', correct: false },
         ]
     },
     {
-        question: "CCS q5",
+        question: "How do you select elements with the class name 'test'?",
         answers: [
-            { text: '&lt;important&gt;', correct: false },
-            { text: 'CCS q5', correct: true },
-            { text: '&lt;i&gt;', correct: false },
-            { text: '&lt;b&gt;', correct: false },
+            { text: '*test', correct: false },
+            { text: 'test', correct: false },
+            { text: '.test', correct: true },
+            { text: '#test', correct: false },
+        ]
+    },
+    {
+        question: "How do you select an element with id 'demo'?",
+        answers: [
+            { text: '.demo', correct: false },
+            { text: '#demo', correct: true },
+            { text: '*demo', correct: false },
+            { text: 'Demo', correct: false },
+        ]
+    },
+    {
+        question: "How do you make each word in a text start with a capital letter?",
+        answers: [
+            { text: 'text-transform:capitalize', correct: true },
+            { text: 'transform:capitalize', correct: false },
+            { text: 'You cannot do that with CSS', correct: false },
+            { text: 'Text-style:capitalize', correct: false },
+        ]
+    },
+    {
+        question: "How do you display hyperlinks without an underline?",
+        answers: [
+            { text: '{text-decoration:none;}', correct: true },
+            { text: '{underline:none;}', correct: false },
+            { text: '{text-decoration:no-underline;}', correct: false },
+            { text: '{decoration:no-underline;}', correct: false },
+        ]
+    },
+    {
+        question: "Which CSS property controls the text size?",
+        answers: [
+            { text: 'font-size', correct: true },
+            { text: 'text-size', correct: false },
+            { text: 'font-style', correct: false },
+            { text: 'Text-style', correct: false },
+        ]
+    },
+    {
+        question: "How do you insert a comment in a CSS file?",
+        answers: [
+            { text: '/* this is a comment */', correct: true },
+            { text: 'this is a comment', correct: false },
+            { text: '// this is a comment', correct: false },
+            { text: '// this is a comment //', correct: false },
         ]
     }
 ];
@@ -123,48 +213,93 @@ let cssQuestions = [
 
 let jsQuestions = [
     {
-        question: "js q1?",
+        question: "Inside which HTML element do we put in JavaScript?",
         answers: [
-            { text: 'js q1', correct: true },
-            { text: 'Home Tool Markup Language', correct: false },
-            { text: 'Hyperlinks and Text Markup Language', correct: false },
-            { text: 'Hyper Type Markup Language', correct: false },
+            { text: '&lt;scripting&gt;', correct: false },
+            { text: '&lt;javaScript&gt;', correct: false },
+            { text: '&lt;script&gt;', correct: true },
+            { text: '&lt;js&gt;', correct: false },
         ]
     },
     {
-        question: "js q2?",
+        question: "How do you write 'Hello World' in an alert box?",
         answers: [
-            { text: '<', correct: false },
-            { text: '*', correct: false },
-            { text: '^', correct: false },
-            { text: 'js q2', correct: true },
+            { text: 'msgBox(“Hello World!”);', correct: false },
+            { text: 'msg(“Hello World!”);', correct: false },
+            { text: 'alert(“Hello World!”);', correct: true },
+            { text: 'alertbox(“Hello World!”);', correct: false },
         ]
     },
     {
-        question: "js q3?",
+        question: "How do you create a function in JavaScript?",
         answers: [
-            { text: 'js q3', correct: true },
-            { text: 'Mozilla', correct: false },
-            { text: 'Google', correct: false },
-            { text: 'Microsoft', correct: false },
+            { text: 'function:myFunction()', correct: false },
+            { text: 'function=myFunction()', correct: false },
+            { text: 'function myFunction()', correct: true },
+            { text: 'function myFunction', correct: false },
         ]
     },
     {
-        question: "js q4",
+        question: "How to write an IF statement in JavaScript?",
         answers: [
-            { text: '&lt;head&gt;', correct: false },
-            { text: "&lt;heading&gt;", correct: false },
-            { text: 'js q4', correct: true },
-            { text: '&lt;h6&gt;', correct: false },
+            { text: 'If i=5 then', correct: false },
+            { text: 'If (i==5)', correct: true },
+            { text: 'If i==5 then', correct: false },
+            { text: 'If i=5', correct: false },
         ]
     },
     {
-        question: "js q5",
+        question: "How does a WHILE loop start?",
         answers: [
-            { text: '&lt;important&gt;', correct: false },
-            { text: 'js q5', correct: true },
-            { text: '&lt;i&gt;', correct: false },
-            { text: '&lt;b&gt;', correct: false },
+            { text: 'While i=1 to 10', correct: false },
+            { text: 'While (i&lt;=10;i++)', correct: false },
+            { text: 'while(i&lt;=10)', correct: true },
+            { text: 'While i&lt;=10', correct: false },
+        ]
+    },
+    {
+        question: "How does a FOR loop start?",
+        answers: [
+            { text: 'for(i=0;i&lt;=5)', correct: false },
+            { text: 'for (i=0; i&lt;=5; i++)', correct: true },
+            { text: 'for (i&lt;=5; i++)', correct: false },
+            { text: 'for i = 1 to 5', correct: false },
+        ]
+    },
+    {
+        question: "How can you add a single line comment in JavaScript?",
+        answers: [
+            { text: 'This is a comment', correct: false },
+            { text: '&lt;!--This is a comment &gt;', correct: false },
+            { text: '//This is a comment', correct: true },
+            { text: '#This is a comment', correct: false },
+        ]
+    },
+    {
+        question: "What is the correct way to write a JavaScript array?",
+        answers: [
+            { text: 'let colors =(1:”red”, 2:”green”, 3:”blue”)', correct: false },
+            { text: 'let colors =[”red”, ”green”,”blue”]', correct: true },
+            { text: 'let colors = ”red”, ”green”,”blue”', correct: false },
+            { text: 'let colors = 1 = ”red”,2 = ”green”,3=”blue”', correct: false },
+        ]
+    },
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        answers: [
+            { text: '-', correct: false },
+            { text: "*", correct: false },
+            { text: '/', correct: false },
+            { text: '=', correct: true },
+        ]
+    },
+    {
+        question: "How do you round the number 5.25, to the nearest integer?",
+        answers: [
+            { text: 'Math.rnd(5.25)', correct: false },
+            { text: 'round(5.25)', correct: false },
+            { text: 'Math.round(5.25)', correct: true },
+            { text: 'rnd(5.25)', correct: false },
         ]
     }
 ];
@@ -338,13 +473,36 @@ function displayScore() {
         document.getElementById('quiz').style.display = 'none';
         document.getElementById('result').style.display = 'block';
     }
-    if (score === 5) {
+    /*if (score === 10) {
         finalScore.innerHTML = `Awesome! You nailed it! Your score is ${score} out of ${shuffledQuestions.length}!`;
-    } else if (score === 3 || score === 4) {
-        finalScore.innerHTML = `Congratulations! you have scored ${score} out of ${shuffledQuestions.length}! You are almost there, Try again`;
-    } else if (score < 3) {
+    } else if (score === 7 || score === 8 || score === 9) {
+        finalScore.innerHTML = `Congratulations! you have scored ${score} out of ${shuffledQuestions.length}! Your knowledge about this topic is little close to 100%, Try again`;
+    } else if (score === 4 || score === 5 || score === 6) {
+        finalScore.innerHTML = `Your Score is ${score} out of ${shuffledQuestions.length}! Give a try again to score more.`;
+    } else if (score === 3 || score === 2 || score === 1) {
         finalScore.innerHTML = `Your score is ${score} out of ${shuffledQuestions.length}! Goodluck for your next attempt.`;
+    } else if (score === 0) {
+        finalScore.innerHTML = `Oops! sorry your score is ${score} out of ${shuffledQuestions.length}!.Better luck next time.`;
+    }*/
+    const scoreRanges = [
+        { minScore: 10, maxScore: 10, message: `Awesome! You nailed it! Your score is ${score} out of ${shuffledQuestions.length}!` },
+        { minScore: 7, maxScore: 9, message: `Congratulations! you have scored ${score} out of ${shuffledQuestions.length}! Your knowledge about this topic is almost close to 100%, Try again` },
+        { minScore: 4, maxScore: 6, message: `Your Score is ${score} out of ${shuffledQuestions.length}! Give a try again to score more.` },
+        { minScore: 1, maxScore: 3, message: `Your score is ${score} out of ${shuffledQuestions.length}! Goodluck for your next attempt.` },
+        { minScore: 0, maxScore: 0, message: `Oops! sorry your score is ${score} out of ${shuffledQuestions.length}!.Better luck next time.` }
+    ];
+    
+    let message = '';
+    
+    for (const range of scoreRanges) {
+        if (score >= range.minScore && score <= range.maxScore) {
+            message = range.message;
+            break;
+        }
     }
+    
+    finalScore.innerHTML = message;
+    
     replayButton.addEventListener('click', playAgain);
 }
 
